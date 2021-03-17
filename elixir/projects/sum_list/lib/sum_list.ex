47a9b9d398357220ce2f1fb4ce58aeb8ac3250d6
Vinicius Estevam <estevamvinicius31@gmail.com>
2021-03-16 21:32:35 -0300

    elixir: First project

---
+++ elixir/projects/sum_list/lib/sum_list.ex
@@ -0,0 +1,12 @@
+defmodule SumList do
+  def call(list), do: sum(list, 0)
+
+  def call_enum(list), do: Enum.map(list, fn elem -> elem > 5 end)
+
+  defp sum([], acc), do: acc
+
+  defp sum([head | tail], acc) do
+    acc = acc + head
+    sum(tail, acc)
+  end
+end
---
+++ elixir/projects/sum_list/test/sum_list_test.exs
@@ -0,0 +1,14 @@
+defmodule SumListTest do
+  use ExUnit.Case
+
+  describe "call/1" do
+    test "returns the list sum" do
+      list = [1, 2, 3]
+
+      response = SumList.call(list)
+      expected_response = 6
+
+      assert response == expected_response
+    end
+  end
+end
